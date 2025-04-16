dfdfd5109813d29c00844e02f134e84ce013f5c1
dnaHunter <daniel.aston@live.co.uk>
2025-04-16 16:08:56 +0100

    Create homepage structure and initial style

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,16 @@
+import SimpleTask from "../../componets/SimpleTask/SimpleTask";
 import "./Homepage.scss";
 
 export default function Homepage() {
   return (
     <section className="home">
-      <h2 className="home__title">All Tasks</h2>
-      <section className="home__list">
-        {}
-      </section>
+      <div className="home__width">
+        <h2 className="home__title">All Caseworker Tasks</h2>
+        <section className="home__list">
+          {<SimpleTask task={{ title: "Task 1", status: "pending" }} />}
+          <div className="home__listBorder"></div>
+        </section>
+      </div>
     </section>
   );
 }
