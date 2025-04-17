cd4ab9bd613467bdbbbe59f37bec9c8f116b19af
dnaHunter <daniel.aston@live.co.uk>
2025-04-17 15:26:55 +0100

    Finished inital styling for the homepage

--- src/componets/SimpleTask/SimpleTask.jsx
+++ src/componets/SimpleTask/SimpleTask.jsx
@@ -2,13 +2,15 @@ import "./SimpleTask.scss";
 import dayjs from "dayjs";
 
 export default function SimpleTask({ task }) {
+  console.log(task);
+
   return (
     <section className="task">
       <div className="task__width">
         <p className="task__title">{task.title}</p>
         <p className="task__status">Status: {task.status}</p>
-        <p className="task__duDate">
-          Due Date: {dayjs(task.dueDate).format("DD/MM/YYYY")}
+        <p className="task__dueDate">
+          Due Date: {dayjs(task.due_date).format("DD/MM/YYYY")}
         </p>
       </div>
     </section>
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,46 @@
+import axios from "axios";
 import SimpleTask from "../../componets/SimpleTask/SimpleTask";
 import "./Homepage.scss";
+import { useEffect, useState } from "react";
 
 export default function Homepage() {
+  const [tasks, setTasks] = useState(null);
+  const [error, setError] = useState(false);
+
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  async function getTasks() {
+    try {
+      const { data } = await axios.get(`${BACKEND_URL}/tasks`);
+
+      setTasks(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!tasks) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section className="home">
       <div className="home__width">
         <h2 className="home__title">All Caseworker Tasks</h2>
+        <p className="home__subtitle">Click on a case for more details or to update it.</p>
+        <p className="home__button">Create new task</p>
         <section className="home__list">
-          {<SimpleTask task={{ title: "Task 1", status: "pending" }} />}
+          {tasks.map((task) => (
+            <SimpleTask task={task} />
+          ))}
           <div className="home__listBorder"></div>
         </section>
       </div>
